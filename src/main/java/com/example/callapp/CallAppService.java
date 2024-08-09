ab794f77d3f0d21a37eb35a71a91f4f4354cf891
Hlib Chibichian <chibgleb99@gmail.com>
2024-08-09 11:52:53 +0100

    call app

---
+++ src/main/java/com/example/callapp/CallAppService.java
@@ -0,0 +1,25 @@
+package com.example.callapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class CallAppService {
+
+    private final RestTemplate restTemplate;
+
+    private String url = "https://qrcodegenbot.onrender.com/3d7b43e3-8b70-4ee2-ac11-f3e33de19417";
+
+    @Scheduled(fixedRate = 10000)
+    public void callExternalApp() {
+        try {
+            String response = restTemplate.getForObject(url, String.class);
+            System.out.println("Response from external: " + response);
+        } catch (Exception ignore) {
+        }
+    }
+
+}
